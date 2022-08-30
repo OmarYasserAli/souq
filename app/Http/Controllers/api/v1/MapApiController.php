8f8f9480e6b794764bfbfa7842ea8d64f4cb40e5
omarYasserAli <3omaryasser74@gmail.com>
2022-08-30 13:56:24 +0200

    s

--- app/Http/Controllers/api/v1/MapApiController.php
+++ app/Http/Controllers/api/v1/MapApiController.php
@@ -8,7 +8,8 @@ use App\Model\BusinessSetting;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Validator;
 use App\CPU\Helpers;
-
+use App\Model\Country;
+use App\Model\Government;
 class MapApiController extends Controller
 {
     public function place_api_autocomplete(Request $request)
@@ -65,9 +66,45 @@ class MapApiController extends Controller
         }
         $api_key = Helpers::get_business_settings('map_api_key');
         $api_key = 'AIzaSyDfnUAEQtTSJ1ca2GZKF0_MPc16K6MixlI';
-        $response = Http::get('https://maps.googleapis.com/maps/api/geocode/json?latlng=' . $request->lat . ',' . $request->lng . '&key=' . $api_key.'&language=us_en&sensor=false');
+        $response = Http::get('https://maps.googleapis.com/maps/api/geocode/json?latlng=' . $request->lat . ',' . $request->lng . '&key=' . $api_key.'&region=EN&language=us_en&sensor=false');
         $res = (($response->json()));
         $country_code = ($res['results'][0]['address_components'][3]['short_name']);
-        return $response->json();
+
+    $data_of_country = [];
+        foreach ($res['results'][1]['address_components'] as $comp) {
+            //loop through each component in ['address_components']
+            foreach ($comp['types'] as $currType){
+                //for every type in the current component, check if it = the check
+                if($currType == 'administrative_area_level_1'){
+
+                    $data_of_country['long_name'] = $comp['long_name'];
+                    //Do whatever with the component, print longname, whatever you need
+                    //You can add $comp into another array to have an array of 'administrative_area_level_1' types
+                }
+                if($currType == 'country'){
+                    $data_of_country['short_name'] = $comp['short_name'];
+
+                    //Do whatever with the component, print longname, whatever you need
+                    //You can add $comp into another array to have an array of 'administrative_area_level_1' types
+                }
+            }
+        }
+          $country_of_long_name = Country::where("code",$data_of_country['short_name'])->first();
+
+           $get_mohafza = explode(" ",$data_of_country['long_name']);
+           if (count($get_mohafza) == 3 ){
+               $get_mohafza = $get_mohafza[1];
+           }else{
+               $get_mohafza = $get_mohafza[0];
+
+           }
+        $goverment_of_long_name = Government::where("country_id",$country_of_long_name->id)->where("name",$get_mohafza)->first();
+        return response()->json([
+            'status' => 200,
+            'message' => 'success',         
+            'goverment' => $goverment_of_long_name,
+            'country' => $country_of_long_name,
+            
+        ]);
     }
 }
